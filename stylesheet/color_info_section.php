06c942953d19d24b6bdedb79f970c2f320da4e73
ABISHRAJ <42835082+ABISHRAJ19@users.noreply.github.com>
2021-03-21 21:39:31 +0530

    Add files via upload

---
+++ stylesheet/color_info_section.php
@@ -0,0 +1,38 @@
+<div class="container">
+
+  <div class="textcolor color-column">
+    <h3 class="textsize">BASE & SURFACE</h3><br><br>
+    <pre ><span class="bg-background-base background-base" >#000000</span><span class="textsize"> Background_Base</span></pre><br><br>
+    <br><br>
+    <pre ><span class="bg-surface-primary surface-primary " >#212121</span><span class="textsize"> Surface_Primary</span></pre><br><br>
+  </div>
+
+  <div class="textcolor color-column">
+    <h3 class="textsize">BRAND COLOUR</h3><br><br>
+    <pre ><span  class="bg-brand-primary brand-primary" >AA1721</span> <span class="textsize">Brand_Primary</span></pre><br><br><br><br>
+    <pre ><span  class="bg-brand-secondary brand-secondary" >17AAA0</span> <span class="textsize">Brand_Secondary</span></pre><br><br><br><br>
+    <pre ><span  class="bg-brand-tertiary brand-tertiary" >E87921</span> <span class="textsize">Brand_Tertiary</span></pre>
+  </div>
+
+  <div class="textcolor color-column">
+    <h3  class="textsize">EMPHASIS COLOUR</h3><br><br>
+    <pre ><span  class="fg-emphasis-low emphasis-low" >646464</span>   <span class="textsize">Emphasis colour_Disabled</span></pre><br><br><br><br>
+    <pre ><span  class="fg-emphasis-medium emphasis-medium" >BDBDBD</span>  <span class="textsize">Emphasis colour_Medium_Emphasis</span></pre><br><br><br><br>
+    <pre ><span  class="fg-emphasis-high emphasis-high" >F5F5F5</span>  <span class="textsize">Emphasis colour_High_Emphasis</span></pre><br><br>
+  </div>
+
+  <div class="textcolor color-column">
+    <h3 class="textsize">ON PRIMARY_COLOUR</h3><br><br>
+    <pre ><span  class="on_Primary_disabled Primary_disabled " >CC9497</span> <span class="textsize">On primary_Disabled</span></pre><br><br><br><br>
+    <pre ><span  class="on_primary_medium primary_medium">FFB9BE</span> <span class="textsize">On primary_Medium_Emphasis</span></pre><br><br><br><br>
+    <pre ><span  class="on_primary_high primary_high txt-color" >FFFFFF</span> <span class="textsize">On primary_High_Emphasis</span></pre>
+  </div>
+
+  <div class="textcolor color-column">
+    <h3 class="textsize">OVERLAYS</h3><br><br>
+    <pre ><span  class="bg-opacity-25 overlay_low" > 38%  </span> <span class="textsize"class="textsize"class="textsize">Overlay_Low</span></pre><br><br><br><br>
+    <pre ><span  class="bg-opacity-50 overlay_medium"> 58%  </span> <span class="textsize"class="textsize">Overlay_Medium</span></pre><br><br><br><br>
+    <pre ><span  class="bg-opacity-80 overlay_high" > 87%  </span> <span class="textsize">Overlay_High</span></pre><br><br><br><br>
+  </div>
+
+</div>
